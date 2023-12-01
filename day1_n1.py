4ebc3f845b65833185a1a87e241d113e4d56d36e
Simon-Deh <152650688+Simon-Deh@users.noreply.github.com>
2023-12-01 22:11:09 +0100

    Create day1_n1.py

---
+++ day1_n1.py
@@ -0,0 +1,23 @@
+for word in sys.stdin:
+    first_digit = 0
+    last_digit = 0
+    for char in word:
+        if char.isdigit():
+            first_digit = char
+            break
+    print('first digit', first_digit)
+    
+    for char in reversed(word):
+        if char.isdigit():
+            last_digit = char
+            break
+    print('last digit', last_digit)
+    
+    number = str(first_digit) + str(last_digit)
+    print('number', number)
+
+    number_to_add = int(number)
+    sum_of_numbers = sum_of_numbers + number_to_add
+    print("sum=", sum_of_numbers)
+    print()
+  
